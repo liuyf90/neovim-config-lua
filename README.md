ac48ce45a9f3cf486406ff5c96f1e89100f860a3
liuyf90 <liuyf90@gmail.com>
2023-02-23 22:29:56 +0800

    add galaxy-line and some ..

--- README.md
+++ README.md
@@ -1,9 +1,38 @@
 # neovim-config-lua
 
-lsp
+## lsp 需要的依赖
+
   -jdsl需要
 	jdk 19
 	python3.11
 
 
-##如何增加plug
+## 如何增加plug
+
+```
+|- lua
+|  |- packer.lua
+\- init.lua
+```
+
+在packer.lua中增加插件
+
+```
+   use {
+    'goolord/alpha-nvim',
+    config = function ()
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+} 
+```
+然后执行
+
+```
+    :so 
+    :PackerSync
+```
+执行之后会自动安装插件，但执行完后透明背景会消失，只要执行如下代码即可
+
+```
+    :lua ColorMyPencils()
+```
--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -65,4 +65,20 @@ return require('packer').startup(function(use)
         },
         tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
+    -- Dashboard
+    use {
+        'goolord/alpha-nvim',
+        config = function()
+            require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+        end
+    }
+    use({
+        "NTBBloodbath/galaxyline.nvim",
+        -- your statusline
+        config = function()
+            require("galaxyline.themes.eviline")
+        end,
+        -- some optional icons
+        requires = { "kyazdani42/nvim-web-devicons", opt = true }
+    })
 end)
