d440d82a0ecd8f625a5ad846a61258737d2963df
liuyf90 <liuyf90@gmail.com>
2024-05-07 15:21:19 +0800

    add a Description on nvim-cmp

--- lua/theflynn/packer.lua
+++ lua/theflynn/packer.lua
@@ -48,6 +48,7 @@ return require('packer').startup(function(use)
 			 {'williamboman/mason-lspconfig.nvim'},
 
 			{'neovim/nvim-lspconfig'},
+            --补全引擎
 			{'hrsh7th/nvim-cmp'},
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'L3MON4D3/LuaSnip'},
