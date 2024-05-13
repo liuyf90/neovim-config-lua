96bb7cc05b4f2fe0aafd418160ff2471f93ae7c8
liuyf90 <liuyf90@gmail.com>
2024-05-13 10:32:29 +0800

    nvimplug initially appears in the top right corner and is floating.

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -5,25 +5,53 @@
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
 
 -- OR setup with some options
-require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive",
-    },
-    view = {
-        width = 30,
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
-})
+--require("nvim-tree").setup {
+--  view = {
+--    side = "right",
+--    float = {
+--      enable = true,
+--      quit_on_focus_loss = false,
+--    },
+--  },
+--}
+local WIDTH_RATIO = 0.3
+local HEIGHT_RATIO = 0.8
+local OFFSET = 3
 
+require('nvim-tree').setup({
+  view = {
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+
+        -- adjust for the offset
+        local col_right_aligned = screen_w - window_w_int - OFFSET
+        local row_offset = OFFSET - 3
+
+
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = row_offset,
+          col = col_right_aligned,
+          width = window_w_int,
+          height = window_h_int,
+        }
+      end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+  },
+})
 
 vim.keymap.set('n', 'tt', ":NvimTreeToggle<CR>")
 vim.keymap.set('n', 'tf', ":NvimTreeFindFile<CR>")
