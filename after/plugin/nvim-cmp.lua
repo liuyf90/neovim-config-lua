b58b488379ac91bca86aa53c8ff613d7df4c8092
liuyf90 <liuyf90@gmail.com>
2024-06-06 08:45:27 +0800

    change keyshort

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -11,7 +11,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<leader><Space>'] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
     ['<Tab>'] = cmp.mapping(function(fallback)
@@ -31,13 +31,13 @@ cmp.setup({
       else
         fallback()
       end
-    end, { 'i', 's' }),
+   end, { 'i', 's' }),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   }, {
     { name = 'buffer' },
-  })
+  }),
 })
 
