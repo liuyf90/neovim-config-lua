c22fa572d3181f9a408989d03edc589a31b8e7e2
liuyf90 <liuyf90@gmail.com>
2024-06-06 10:39:27 +0800

    modifty vue

--- after/plugin/vue.lua
+++ after/plugin/vue.lua
@@ -1,31 +1,32 @@
--- ~/.config/nvim/lua/lsp/vue.lua
+local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-
-  local lspconfig = require('lspconfig')
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-  lspconfig.vuels.setup {
-    capabilities = capabilities,
-    filetypes = { 'vue', 'html' }, -- 支持 vue 和 html 文件
-    init_options = {
-      config = {
-        vetur = {
-          useWorkspaceDependencies = true,
-          validation = {
-            template = true,
-            script = true,
-            style = true,
-            templateProps = true,
-            interpolation = true
-          },
-          completion = {
-            autoImport = true,
-            tagCasing = 'kebab',
-            useScaffoldSnippets = true
-          },
-        }
-      }
+lspconfig.volar.setup {
+  capabilities = capabilities,
+  filetypes = { 'typescript', 'html', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  init_options = {
+    typescript = {
+      tsdk = '/path/to/your/typescript/lib' -- optional, you can leave it empty
+    },
+    languageFeatures = {
+      implementation = true, -- enable 'go to implementation'
+      references = true,
+      definition = true,
+      typeDefinition = true,
+      callHierarchy = true,
+      hover = true,
+      rename = true,
+      renameFileRefactoring = true,
+      signatureHelp = true,
+      codeAction = true,
+      workspaceSymbol = true,
+      completion = {
+        defaultTagNameCase = 'both',
+        defaultAttrNameCase = 'kebab',
+        getDocumentNameCasesRequest = false,
+        getDocumentSelectionRequest = false,
+      },
     }
   }
-
+}
 
